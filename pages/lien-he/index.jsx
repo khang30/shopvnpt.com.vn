be38530410724af9d97903e0e6d9b68e5e001579
khang30 <139552977+khang30@users.noreply.github.com>
2023-09-07 14:07:15 +0700

    Update index.jsx

--- pages/lien-he/index.jsx
+++ pages/lien-he/index.jsx
@@ -13,7 +13,7 @@ function LienHe() {
           <div className="flex flex-col gap-5 pt-10">
             <div>
               <span className="block text-lg font-bold">
-                Website PGD Kinh doanh:{" "}
+                Website PGD Kinh doanh PBH:{" "}
               </span>
               <span className="text-text">Trương Minh Tâm</span>
             </div>
@@ -34,7 +34,7 @@ function LienHe() {
               </span>
             </div>
             <iframe
-              src="https://goo.gl/maps/wyYmLTf8S7vh7o72A"
+              src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.6154964461007!2d106.6789866!3d10.764087700000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f1e617e7577%3A0xb47d54b07c95b307!2zMiDEkC4gSMO5bmcgVsawxqFuZywgUGjGsOG7nW5nIDEsIFF14bqtbiAxMCwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5o!5e0!3m2!1svi!2s!4v1694070360705!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
               width="full"
               height="500"
               style={{ border: 0 }}
