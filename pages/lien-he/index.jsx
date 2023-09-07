6a5f22022566973ceecaaa7119481ed25f7720f0
khang30 <139552977+khang30@users.noreply.github.com>
2023-09-07 14:11:35 +0700

    Update index.jsx

--- pages/lien-he/index.jsx
+++ pages/lien-he/index.jsx
@@ -34,7 +34,7 @@ function LienHe() {
               </span>
             </div>
             <iframe
-              src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.6154964461007!2d106.6789866!3d10.764087700000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f1e617e7577%3A0xb47d54b07c95b307!2zMiDEkC4gSMO5bmcgVsawxqFuZywgUGjGsOG7nW5nIDEsIFF14bqtbiAxMCwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5o!5e0!3m2!1svi!2s!4v1694070360705!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.6154964461007!2d106.6789866!3d10.764087700000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f1e617e7577%3A0xb47d54b07c95b307!2zMiDEkC4gSMO5bmcgVsawxqFuZywgUGjGsOG7nW5nIDEsIFF14bqtbiAxMCwgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5o!5e0!3m2!1svi!2s!4v1694070360705!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
               width="full"
               height="500"
               style={{ border: 0 }}
