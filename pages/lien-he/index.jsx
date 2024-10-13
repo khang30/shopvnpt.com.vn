f48879f9b5ea4ffd5568621e602a942b6e0e9107
khang30 <139552977+khang30@users.noreply.github.com>
2024-10-13 10:25:44 +0700

    Update index.jsx

--- pages/lien-he/index.jsx
+++ pages/lien-he/index.jsx
@@ -33,15 +33,7 @@ function LienHe() {
                 Hotline: 0913887868 <br /> Mail: 
               </span>
             </div>
-            <iframe
-              src="https://maps.app.goo.gl/GBanhcQnUQ4abeLu6"
-              width="full"
-              height="500"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3917.748387726419!2d106.68336717428926!3d10.906716948043353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174d7b0f0ba245b%3A0xe73d55e6dd497ff2!2zMTIgQsO5aSBDw7RuZyBUcuG7q25nLCBOaOG7iyBCw6xuaCwgSMOzYyBNw7RuLCBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1728789467571!5m2!1svi!2s" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
       </Layout>
